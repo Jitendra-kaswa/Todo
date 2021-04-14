5200cd9423d899f649be58e9c524c0468a360f6f
Jitendra Kaswa <kaswanjitendra89@gmail.com>
2021-04-14 17:18:12 +0530

    basic layout added

---
+++ assets/js/home.js
@@ -0,0 +1,1 @@
+console.log("Inside Home Section")
--- server.js
+++ server.js
@@ -1,9 +1,29 @@
 const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const path = require('path');
+const cookieParser = require('cookie-parser');
 const port = 8000;
 const app= express();
 
-app.get('/',(req,res)=>{
-    return res.send("Hello Mitro");
-})
+// import sass into project
+const sassMiddleware = require('node-sass-middleware');
+app.use(sassMiddleware({
+    src:'./assets/scss',
+    dest:'./assets/css', 
+    debug:true, 
+    outputStyle:'extended',
+    prefix:'/css'
+}))
+
+// setup the view engine
+app.set('view engine','ejs');
+app.set('views','./views');
+
+app.use(expressLayouts);
+app.use(express.urlencoded({extended:true}));
+app.use(cookieParser());
+app.use(express.static('assets'));
+
+app.use('/',require('./routers'));
 
 app.listen(port);
